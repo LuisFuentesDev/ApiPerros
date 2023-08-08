b9757dfddcbf52894b510350cec30c5222032b04
LuisFuentesDev <lsfuentes2@gmail.com>
2023-08-07 22:21:48 -0400

    Avances del view model

--- app/src/main/java/com/example/apiperros/presentacion/RazaViewModel.kt
+++ app/src/main/java/com/example/apiperros/presentacion/RazaViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.apiperros.presentacion
 
-class RazaViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.apiperros.data.Repositorio
+import com.example.apiperros.data.local.RazaDataBase
+import com.example.apiperros.data.remote.RazaPerritoRetrofit
+import kotlinx.coroutines.launch
+
+class RazaViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repositorio: Repositorio
+
+    init {
+        val api = RazaPerritoRetrofit.getRetrofitRaza()
+        val razaDataBase = RazaDataBase.getDataBase(application).getRazaDao()
+        repositorio = Repositorio(api, razaDataBase)
+
+    }
+
+    fun getAllRazas() = viewModelScope.launch {
+        repositorio.getRazas()
+    }
 }
