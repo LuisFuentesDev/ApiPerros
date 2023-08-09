b9805305be7dee8d94af49faa120aa42cf8ec079
LuisFuentesDev <lsfuentes2@gmail.com>
2023-08-09 19:23:16 -0400

    se agrega navegacion a fragmento detalle

--- app/src/main/java/com/example/apiperros/presentacion/AdapterRazas.kt
+++ app/src/main/java/com/example/apiperros/presentacion/AdapterRazas.kt
@@ -2,7 +2,9 @@ package com.example.apiperros.presentacion
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.example.apiperros.R
 import com.example.apiperros.data.local.RazaEntity
 import com.example.apiperros.databinding.ItemRazasBinding
 
@@ -11,8 +13,11 @@ class AdapterRazas : RecyclerView.Adapter<AdapterRazas.ItemRazasViewHolder>() {
     lateinit var binding: ItemRazasBinding
     private val listItemRazas = mutableListOf<RazaEntity>()
 
-    override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): AdapterRazas.ItemRazasViewHolder {
-       binding = ItemRazasBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AdapterRazas.ItemRazasViewHolder {
+        binding = ItemRazasBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ItemRazasViewHolder(binding)
     }
 
@@ -25,15 +30,20 @@ class AdapterRazas : RecyclerView.Adapter<AdapterRazas.ItemRazasViewHolder>() {
         holder.bind(raza)
     }
 
-    fun setData(razas:List<RazaEntity>){
+    fun setData(razas: List<RazaEntity>) {
         this.listItemRazas.clear()
         this.listItemRazas.addAll(razas)
         notifyDataSetChanged()
     }
 
-    class ItemRazasViewHolder(val razasVista:ItemRazasBinding):RecyclerView.ViewHolder(razasVista.root) {
-        fun bind(raza:RazaEntity){
-        razasVista.txtRaza.text = raza.raza
+    class ItemRazasViewHolder(val razasVista: ItemRazasBinding) :
+        RecyclerView.ViewHolder(razasVista.root) {
+        fun bind(raza: RazaEntity) {
+            razasVista.txtRaza.text = raza.raza
+            razasVista.cardView.setOnClickListener {
+                Navigation.findNavController(razasVista.root)
+                    .navigate(R.id.action_listadoRazas_to_fragmentDetalle)
+            }
         }
     }
 }
